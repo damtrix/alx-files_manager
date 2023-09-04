4ff0de88d3ae81dc36fe5c411013b7382baa8721
damtrix <oludamola.onarinde@gmail.com>
2023-09-04 13:28:18 +0100

    push

---
+++ server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* eslint-disable comma-dangle */
+
+const express = require('express');
+const router = require('./routes/index');
+
+const server = express();
+const PORT = process.env.PORT ? process.env.PORT : 5000;
+
+server.use(express.json());
+server.use(router);
+
+server.listen(
+  PORT,
+  () => console.log(`The server is running on port: ${PORT}`)
+  // eslint-disable-next-line function-paren-newline
+);
